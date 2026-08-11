6f76f5bfe7f8916fac82da0c5a42a0841a537ec5
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-11 11:11:18 +0530

    Add files via upload

---
+++ Withoutreturnwithargument.java
@@ -0,0 +1,15 @@
+package Day08;
+
+public class Withoutreturnwithargument {
+	public void addvalues(int a, int b){
+        int sum=a+b;
+        System.out.println("Result:"+sum);
+    }
+    public static void main(String[] args) {
+        Withoutreturnwithargument obj=new Withoutreturnwithargument();
+        obj.addvalues(5,8);
+    obj.addvalues(10,20);
+    }
+}
+
+
