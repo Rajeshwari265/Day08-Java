c64d30848eacc83ac30b65da2a997e26d00e06dc
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-11 10:18:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Day08;
+
+public class Main {
+	    public int addValues(){
+	        int a=5;int b=8;
+	        int sum =a+b;
+	        return sum;
+
+	    }
+	    public static void main(String[] args) {
+	        Main obj=new Main();
+	        int result=obj.addValues();
+	        System.out.println("Result:"+result);
+	}}
